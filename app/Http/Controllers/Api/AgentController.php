bbd3405231b7dc211a4d395d1436f293cc6a3492
syafrin <syafrinibrahim12@gmail.com>
2020-07-04 21:08:35 -0700

    create agent api

--- app/Http/Controllers/Api/AgentController.php
+++ app/Http/Controllers/Api/AgentController.php
@@ -36,7 +36,7 @@ class AgentController extends Controller
             'alamat' => 'required|max:255',
             'latitude' => 'required|max:255',
             'longitude' => 'required|max:255',
-            'img_toko' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'img_toko' => 'required|image|mimes:jpeg,jpg,png|max:2048'
         ]);
 
         if($validator->fails()){
@@ -76,7 +76,14 @@ class AgentController extends Controller
      */
     public function show($id)
     {
-        //
+        $agent = Agent::find($id);
+        if(is_null($agent)){
+            return response()->json([
+                'status'=>FALSE,
+                'message'=>'Record Not Found'
+            ], 404);
+        }
+         return new AgentResource($agent);
     }
 
     /**
@@ -88,7 +95,51 @@ class AgentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $data = $request->all();
+        $agent = Agent::findOrFail($id);
+        if(is_null($agent)){
+             return response()->json([
+                'status'=>FALSE,
+                'message'=>'Record Not Found'
+            ], 404);
+        }
+        $validator  = Validator::make($data, [
+            'nama_toko' => 'required|max:255',
+            'nama_pemilik' => 'required|max:255',
+            'alamat' => 'required|max:255',
+            'latitude' => 'required|max:255',
+            'longitude' => 'required|max:255',
+            'img_toko' => 'sometimes|image|mimes:jpeg,jpg,png|max:2048'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => FALSE,
+                'message' => $validator->errors()
+            ], 400);
+        }
+        
+        if($request->hasFile('img_toko')){
+            if($request->file('img_toko')->isValid()){
+
+                Storage::disk('upload')->delete($agent->img_toko);
+
+               $img = $request->file('img_toko');
+               $ext = $img->getClientOriginalExtension();
+               $nm_img = "agent/".date('YmdHis').".".$ext;
+               $path = 'public/uploads/agent';
+               $request->file('img_toko')->move($path, $nm_img);
+               $data['img_toko'] = $nm_img;
+           }
+
+        }
+
+        $agent->update($data);
+        return response()->json([
+            'status' => TRUE,
+            'message' =>  'data berhasil diupdate'
+        ], 200);
+
     }
 
     /**
@@ -99,6 +150,25 @@ class AgentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $agent = Agent::find($id);
+        if(is_null($agent)){
+            return response()->json([
+                'status' => FALSE,
+                'message' => 'Record Not Found'
+            ], 404);
+        }
+        $agent->delete();
+        Storage::disk('upload')->delete($agent->img_toko);  
+        return response()->json([
+            'status' => TRUE,
+            'message' => 'Data berhasil dihapus'
+        ], 200);
+
+    }
+
+    public function search(Request $request){
+        $filter = $request->get('keyword');
+        return AgentResource::collection(Agent::where('nama_toko', 'LIKE', "%$filter%")->get());
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('agent', 'Api\AgentController');
+Route::get('search_agent','Api\AgentController@search');
